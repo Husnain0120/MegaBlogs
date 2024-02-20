9878841d5fa84d7b074c1cebedb7fd57fed1c745
Husnain0120 <144523048+Husnain0120@users.noreply.github.com>
2024-02-20 16:05:01 +0500

    auth update

--- MegaBlogs/src/conf/conf.js
+++ MegaBlogs/src/conf/conf.js
@@ -1,5 +1,7 @@
 const conf = {
-    appwriteUrl:String(import.meta.env.VITE_APPWRITE_URL),
+    
+    appwriteUrl:String(import.meta.env.
+    VITE_APPWRITE_URL),
 
     appwriteProjectId:String(import.meta.env.
     VITE_APPWRITE_PROJECT_ID),
